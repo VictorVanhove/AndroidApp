bdf0e7eb1905baccbedca3fb1e1f4ea50fb43262
Victor Vanhove <victorvanhove@hotmail.com>
2019-08-05 15:39:38 +0200

    Added first implentation in collectionView

--- app/src/main/java/com/example/dikkeploaten/Fragments/CollectionFragment.kt
+++ app/src/main/java/com/example/dikkeploaten/Fragments/CollectionFragment.kt
@@ -6,13 +6,38 @@ import android.os.Bundle
     import android.view.ViewGroup
     import androidx.annotation.NonNull
     import androidx.fragment.app.Fragment
-    import com.example.dikkeploaten.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.dikkeploaten.Adapters.AlbumAdapter
+import com.example.dikkeploaten.Models.Album
+import com.example.dikkeploaten.R
+import kotlinx.android.synthetic.main.fragment_collection.*
 
-    class CollectionFragment : Fragment() {
+class CollectionFragment : Fragment() {
+
+        private lateinit var adapter: AlbumAdapter
 
         override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
             // Inflate the layout for this fragment
             return inflater.inflate(R.layout.fragment_collection, container, false)
+        }
+
+        override fun onActivityCreated(savedInstanceState: Bundle?) {
+            super.onActivityCreated(savedInstanceState)
+
+            val albums = listOf(
+                Album(0,"Highway To Hell","ACDC","bla"),
+                Album(1,"Rumours","Fleetwood Mac","bla"),
+                Album(2,"Heroes","David Bowie","bla"),
+                Album(3,"1999","Prince","bla"),
+                Album(4,"Thriller","Michael Jackson","bla"),
+                Album(5,"Faith","George Michael","bla"),
+                Album(6,"Make It Big","Wham!","bla")
+            )
+
+            adapter = AlbumAdapter(context!!, albums)
+            recyclerView.adapter = adapter
+            recyclerView.setHasFixedSize(true)
+            recyclerView.layoutManager = LinearLayoutManager(context)
         }
 
 }
