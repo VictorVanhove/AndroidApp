6c4e9a84e99044f55996ae5e610d773f12647512
Victor Vanhove <victorvanhove@hotmail.com>
2019-08-06 23:42:20 +0200

    Added same logic for wantListView + layout, API

--- app/src/main/java/com/example/dikkeploaten/Fragments/WantlistFragment.kt
+++ app/src/main/java/com/example/dikkeploaten/Fragments/WantlistFragment.kt
@@ -5,13 +5,38 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.dikkeploaten.Adapters.AlbumAdapter
+import com.example.dikkeploaten.Models.Album
 import com.example.dikkeploaten.R
+import com.example.dikkeploaten.Services.API
+import kotlinx.android.synthetic.main.fragment_collection.*
 
 class WantlistFragment : Fragment() {
+
+    private lateinit var adapter: AlbumAdapter
+    private var albums = arrayListOf<Album>()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_wantlist, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        API.shared().getUserWantlist { albums ->
+            this.albums = albums
+            fillRecyclerView(this.albums)
+        }
+
+        fillRecyclerView(this.albums)
+    }
+
+    private fun fillRecyclerView(albums: ArrayList<Album>) {
+        adapter = AlbumAdapter(context!!, albums)
+        recyclerView.adapter = adapter
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+    }
 }
--- app/src/main/java/com/example/dikkeploaten/Services/API.kt
+++ app/src/main/java/com/example/dikkeploaten/Services/API.kt
@@ -46,6 +46,36 @@ class API {
 
     }
 
+    fun getUserWantlist(callback: (ArrayList<Album>) -> Unit) {
+        var albums = arrayListOf<Album>()
+
+        val userWantlist = db.collection("users").document(auth.currentUser!!.uid).collection("wantList")
+        userWantlist.get().addOnSuccessListener { result ->
+            for (document in result) {
+                var userAlbum = document.toObject(UserAlbum::class.java)
+                db.collection("platen").document(userAlbum.albumID).get()
+                    .addOnSuccessListener { document ->
+                        if (document != null) {
+                            var album = document.toObject(Album::class.java)
+                            album!!.id = document.id
+                            albums.add(album)
+
+                            callback(albums)
+                        } else {
+                            Log.d(TAG, "No such document")
+                        }
+                    }
+                    .addOnFailureListener { exception ->
+                        Log.d(TAG, "get failed with ", exception)
+                    }
+            }
+        }
+            .addOnFailureListener { exception ->
+                Log.d(TAG, "Error getting documents: ", exception)
+            }
+
+    }
+
     fun getAlbumList(callback: (ArrayList<Album>) -> Unit) {
         var albums = arrayListOf<Album>()
         db.collection("platen")
