26063ec51f796f940f2e608db518193e58b9b516
Victor Vanhove <victorvanhove@hotmail.com>
2019-08-12 20:10:42 +0200

    added user model

---
+++ app/src/main/java/com/example/dikkeploaten/Models/User.kt
@@ -0,0 +1,9 @@
+package com.example.dikkeploaten.Models
+
+data class User (
+    var id: String = "",
+    var username: String = "",
+    var email: String = "",
+    var plates: ArrayList<UserAlbum> = arrayListOf(),
+    var wantList: ArrayList<UserAlbum> = arrayListOf()
+)
