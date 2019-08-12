093a310639225884eca158ca678cfb5965034dad
Victor Vanhove <victorvanhove@hotmail.com>
2019-08-12 15:03:24 +0200

    Added cache model

---
+++ app/src/main/java/com/example/dikkeploaten/Services/Cache.kt
@@ -0,0 +1,9 @@
+package com.example.dikkeploaten.Services
+
+import com.example.dikkeploaten.Models.Album
+import com.example.dikkeploaten.Models.User
+
+class Cache {
+    var user: User = User()
+    var albums: ArrayList<Album> = arrayListOf()
+}
