9cfcfb39baa4ebafe15f5f005838d754f94a816e
Victor Vanhove <victorvanhove@hotmail.com>
2019-08-13 17:18:24 +0200

    Fixed changePassword/Username, now fully works

--- app/src/main/java/com/example/dikkeploaten/Fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/dikkeploaten/Fragments/SettingsFragment.kt
@@ -1,22 +1,43 @@
 package com.example.dikkeploaten.Fragments
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.preference.EditTextPreference
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
-import com.example.dikkeploaten.R
 import com.example.dikkeploaten.Services.API
+
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.xml.preferences, rootKey)
+        setPreferencesFromResource(com.example.dikkeploaten.R.xml.preferences, rootKey)
+
 
         val usernamePreference: EditTextPreference? = findPreference("username")
         val passwordPreference: EditTextPreference? = findPreference("password")
 
-        if(usernamePreference?.text != null && passwordPreference?.text != null) {
-            API.shared.updateUsername(usernamePreference.text)
-            API.shared.updatePassword(passwordPreference.text)
+        usernamePreference?.summaryProvider = Preference.SummaryProvider<EditTextPreference> { preference ->
+            val text = preference.text
+            if (TextUtils.isEmpty(text)) {
+                "Not set"
+            } else {
+                API.shared.updateUsername(text)
+                text
+            }
+        }
+        passwordPreference?.summaryProvider = Preference.SummaryProvider<EditTextPreference> { preference ->
+            val text = preference.text
+            if (TextUtils.isEmpty(text)) {
+                "Not set"
+            } else {
+                API.shared.updatePassword(text)
+                text
+            }
+        }
+    }
+
         }
     }
 
