86c5be57e4ec83bee5eacb6592904686bf577d45
Victor Vanhove <victorvanhove@hotmail.com>
2020-01-03 12:28:39 +0100

    Added viewModels from the first 3 tabs

---
+++ app/src/main/java/com/hogent/dikkeploaten/viewmodels/WantlistViewModel.kt
@@ -0,0 +1,95 @@
+package com.hogent.dikkeploaten.viewmodels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.hogent.dikkeploaten.network.AlbumProperty
+import com.hogent.dikkeploaten.network.Api
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+class WantlistViewModel : ViewModel() {
+
+    // The internal MutableLiveData that stores the status of the most recent request
+    private val _status = MutableLiveData<ApiStatus>()
+
+    // The external immutable LiveData for the request status
+    val status: LiveData<ApiStatus>
+        get() = _status
+
+    // Internally, we use a MutableLiveData, because we will be updating the List of MarsProperty
+    // with new values
+    private val _properties = MutableLiveData<List<AlbumProperty>>()
+
+    // The external LiveData interface to the property is immutable, so only this class can modify
+    val properties: LiveData<List<AlbumProperty>>
+        get() = _properties
+
+    // Internally, we use a MutableLiveData to handle navigation to the selected property
+    private val _navigateToSelectedProperty = MutableLiveData<AlbumProperty>()
+
+    // The external immutable LiveData for the navigation property
+    val navigateToSelectedProperty: LiveData<AlbumProperty>
+        get() = _navigateToSelectedProperty
+
+    // Create a Coroutine scope using a job to be able to cancel when needed
+    private var viewModelJob = Job()
+
+    // the Coroutine runs using the Main (UI) dispatcher
+    private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main)
+
+    /**
+     * Call getMarsRealEstateProperties() on init so we can display status immediately.
+     */
+    init {
+        getAlbumProperties()
+    }
+
+    /**
+     * Gets Mars real estate property information from the Mars API Retrofit service and updates the
+     * [MarsProperty] [List] and [MarsApiStatus] [LiveData]. The Retrofit service returns a
+     * coroutine Deferred, which we await to get the result of the transaction.
+     */
+    private fun getAlbumProperties() {
+        coroutineScope.launch {
+            // Get the Deferred object for our Retrofit request
+            var getAlbumsDeferred = Api.retrofitService.getProperties()
+            try {
+                _status.value = ApiStatus.LOADING
+                // this will run on a thread managed by Retrofit
+                val listResult = getAlbumsDeferred.await()
+                _status.value = ApiStatus.DONE
+                _properties.value = listResult
+            } catch (e: Exception) {
+                _status.value = ApiStatus.ERROR
+                _properties.value = ArrayList()
+            }
+        }
+    }
+
+    /**
+     * When the [ViewModel] is finished, we cancel our coroutine [viewModelJob], which tells the
+     * Retrofit service to stop.
+     */
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+
+    /**
+     * When the property is clicked, set the [_navigateToSelectedProperty] [MutableLiveData]
+     * @param marsProperty The [MarsProperty] that was clicked on.
+     */
+    fun displayPropertyDetails(albumProperty: AlbumProperty) {
+        _navigateToSelectedProperty.value = albumProperty
+    }
+
+    /**
+     * After the navigation has taken place, make sure navigateToSelectedProperty is set to null
+     */
+    fun displayPropertyDetailsComplete() {
+        _navigateToSelectedProperty.value = null
+    }
+}
