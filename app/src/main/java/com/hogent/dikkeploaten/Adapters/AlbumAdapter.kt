f5312b99ec9b357173342fb0e82c154552643ac6
Victor Vanhove <victorvanhove@hotmail.com>
2020-01-03 12:32:02 +0100

    Refactored adapter class for recyclerView

--- app/src/main/java/com/hogent/dikkeploaten/Adapters/AlbumAdapter.kt
+++ app/src/main/java/com/hogent/dikkeploaten/Adapters/AlbumAdapter.kt
@@ -1,94 +1,111 @@
 package com.hogent.dikkeploaten.adapters
 
-import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.bumptech.glide.request.RequestOptions
-import com.hogent.dikkeploaten.R
-import com.hogent.dikkeploaten.activities.MainActivity
+import com.hogent.dikkeploaten.databinding.LayoutAlbumItemBinding
 import com.hogent.dikkeploaten.models.Album
-import com.hogent.dikkeploaten.services.API
-import kotlinx.android.synthetic.main.layout_albumitem.view.*
+import com.hogent.dikkeploaten.network.AlbumProperty
 
 /**
  * Adapter class for each album in recyclerView.
  */
-class AlbumAdapter(var context: Context, private var albums: ArrayList<Album>, private var showStatus: Boolean) :
-    RecyclerView.Adapter<AlbumAdapter.AlbumViewHolder>() {
+class AlbumAdapter(val onClickListener: OnClickListener) :
+    ListAdapter<AlbumProperty, AlbumAdapter.AlbumPropertyViewHolder>(DiffCallback) {
 
     /**
-     * Inflates the AlbumViewHolder.
+     * The MarsPropertyViewHolder constructor takes the binding variable from the associated
+     * GridViewItem, which nicely gives it access to the full [MarsProperty] information.
      */
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlbumViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(
-            R.layout.layout_albumitem, parent, false
-        )
-        return AlbumViewHolder(itemView)
+    class AlbumPropertyViewHolder(private var binding: LayoutAlbumItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(albumProperty: AlbumProperty) {
+            binding.property = albumProperty
+            // This is important, because it forces the data binding to execute immediately,
+            // which allows the RecyclerView to make the correct view size measurements
+            binding.executePendingBindings()
+        }
+
+    }
+
+    /**
+     * Allows the RecyclerView to determine which items have changed when the [List] of [MarsProperty]
+     * has been updated.
+     */
+    companion object DiffCallback : DiffUtil.ItemCallback<AlbumProperty>() {
+        override fun areItemsTheSame(oldItem: AlbumProperty, newItem: AlbumProperty): Boolean {
+            return oldItem === newItem
+        }
+
+        override fun areContentsTheSame(oldItem: AlbumProperty, newItem: AlbumProperty): Boolean {
+            return oldItem.id == newItem.id
+        }
+    }
+
+    /**
+     * Create new [RecyclerView] item views (invoked by the layout manager)
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlbumPropertyViewHolder {
+        return AlbumPropertyViewHolder(LayoutAlbumItemBinding.inflate(LayoutInflater.from(parent.context)))
     }
 
     /**
      * Binds each album in the ArrayList to a view.
      */
-    override fun onBindViewHolder(holder: AlbumViewHolder, position: Int) {
-        val album = albums[position]
-        holder.txtTitle.text = album.title
-        holder.txtArtist.text = album.artist
-
-        val requestOptions = RequestOptions().diskCacheStrategy(DiskCacheStrategy.ALL)
-        Glide.with(context).load(album.thumb).apply(requestOptions).into(holder.imageAlbum)
-
-        //Adds status image if album belongs to collection/wantlist
-        if (showStatus) {
-            when {
-                API.shared.cache.user.plates.any { userAlbum -> userAlbum.albumID == album.id } -> Glide.with(context).load(
-                    R.mipmap.ic_in_collection
-                ).apply(requestOptions).into(holder.imageStatus)
-                API.shared.cache.user.wantList.any { userAlbum -> userAlbum.albumID == album.id } -> Glide.with(context).load(
-                    R.mipmap.ic_in_wantlist
-                ).apply(requestOptions).into(holder.imageStatus)
-                else -> Glide.with(context).load(R.drawable.ic_not_in_collection).apply(requestOptions).into(holder.imageStatus)
-            }
-        }
-
+    override fun onBindViewHolder(holder: AlbumPropertyViewHolder, position: Int) {
+        val albumProperty = getItem(position)
         holder.itemView.setOnClickListener {
-            run {
-                if (context is MainActivity) {
-                    val activity = context as MainActivity
-                    activity.goToAlbumDetail(album)
-                }
-            }
+            onClickListener.onClick(albumProperty)
         }
+        holder.bind(albumProperty)
+//        val requestOptions = RequestOptions().diskCacheStrategy(DiskCacheStrategy.ALL)
+//        Glide.with(context).load(album.thumb).apply(requestOptions).into(holder.imageAlbum)
+//
+//        //Adds status image if album belongs to collection/wantlist
+//        if (showStatus) {
+//            when {
+//                API.shared.cache.user.plates.any { userAlbum -> userAlbum.albumID == album.id } -> Glide.with(context).load(
+//                    R.mipmap.ic_in_collection
+//                ).apply(requestOptions).into(holder.imageStatus)
+//                API.shared.cache.user.wantList.any { userAlbum -> userAlbum.albumID == album.id } -> Glide.with(context).load(
+//                    R.mipmap.ic_in_wantlist
+//                ).apply(requestOptions).into(holder.imageStatus)
+//                else -> Glide.with(context).load(R.drawable.ic_not_in_collection).apply(requestOptions).into(holder.imageStatus)
+//            }
+//        }
+//
+//        holder.itemView.setOnClickListener {
+//            run {
+//                if (context is MainActivity) {
+//                    val activity = context as MainActivity
+//                    //activity.goToAlbumDetail(album)
+//                }
+//            }
+//        }
     }
 
     /**
      * Gets the number of albums in the list.
      */
-    override fun getItemCount(): Int {
-        return albums.size
-    }
-
-    /**
-     * Holds the view for the album.
-     */
-    class AlbumViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-        val txtTitle = itemView.title_album!!
-        val txtArtist = itemView.artist_album!!
-        val imageAlbum = itemView.image_album!!
-        val imageStatus = itemView.image_status!!
-
-    }
+    //override fun getItemCount() = albums.size
 
     /**
      * Filters the albums in the recyclerView
      */
     fun setFilter(filteredList: ArrayList<Album>) {
-        this.albums = filteredList
+        //this.albums = filteredList
         notifyDataSetChanged()
+    }
+
+    /**
+     * Custom listener that handles clicks on [RecyclerView] items.  Passes the [MarsProperty]
+     * associated with the current item to the [onClick] function.
+     * @param clickListener lambda that will be called with the current [MarsProperty]
+     */
+    class OnClickListener(val clickListener: (albumProperty: AlbumProperty) -> Unit) {
+        fun onClick(albumProperty: AlbumProperty) = clickListener(albumProperty)
     }
 
 }
