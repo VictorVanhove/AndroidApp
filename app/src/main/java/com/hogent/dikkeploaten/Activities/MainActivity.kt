69875458e0150f8aad90c0cf85bde73f4c83f2e5
Victor Vanhove <victorvanhove@hotmail.com>
2020-07-23 12:02:58 +0200

    Refactored main activity, added binding + changed bottomNav

--- app/src/main/java/com/hogent/dikkeploaten/Activities/MainActivity.kt
+++ app/src/main/java/com/hogent/dikkeploaten/Activities/MainActivity.kt
@@ -1,117 +1,46 @@
 package com.hogent.dikkeploaten.activities
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.hogent.dikkeploaten.R
+import com.hogent.dikkeploaten.databinding.ActivityMainBinding
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 /**
  * Activity class for the main part of the app.
  */
 class MainActivity : AppCompatActivity() {
 
-    /**
-     * Loads the fragment equal to the parameter and updates the container.
-     */
-//    private fun loadFragment(fragment: Fragment): Boolean {
-//        if (fragment != null) {
-//            supportFragmentManager.beginTransaction().replace(com.hogent.dikkeploaten.R.id.fragment_container, fragment)
-//                .commit()
-//            return true
-//        }
-//        return false
-//    }
-
-    /**
-     * Handles actions when an item in the bottom navigation menu was clicked.
-     */
-//    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-//        var fragment = Fragment()
-//        when (item.itemId) {
-//            com.hogent.dikkeploaten.R.id.navigation_collection -> {
-//                fragment = CollectionFragment()
-//            }
-//            com.hogent.dikkeploaten.R.id.navigation_wantlist -> {
-//                fragment = WantlistFragment()
-//            }
-//            com.hogent.dikkeploaten.R.id.navigation_search -> {
-//                fragment = SearchFragment()
-//            }
-//            com.hogent.dikkeploaten.R.id.navigation_notifications -> {
-//                fragment = NotificationsFragment()
-//            }
-//            com.hogent.dikkeploaten.R.id.navigation_profile -> {
-//                fragment = ProfileFragment()
-//            }
-//        }
-//        return loadFragment(fragment)
-//    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-//        if (API.shared.isUserLoggedIn()) {
-////            loadFragment(CollectionFragment())
-////        } else {
-////            val intent = Intent(this, LoginActivity::class.java)
-////            startActivity(intent)
-////            finish()
-////        }
-////
-////        val navView: BottomNavigationView = findViewById(com.hogent.dikkeploaten.R.id.nav_view)
-////
-////        navView.setOnNavigationItemSelectedListener(this)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        setUpNavigation()
-    }
+        val bottomNavigationView = binding.bottomNavView
+        val navController = findNavController(R.id.nav_host_fragment)
 
-    private fun setUpNavigation() {
-        val bottomNavigationView: BottomNavigationView = findViewById(R.id.nav_view)
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment?
-        NavigationUI.setupWithNavController(
-            bottomNavigationView,
-            navHostFragment!!.navController
+        bottomNavigationView.setupWithNavController(navController)
+
+        // Setting Up ActionBar with Navigation Controller
+        var appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = setOf (
+                R.id.collectionFragment,
+                R.id.searchFragment,
+                R.id.wantlistFragment
+            )
         )
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
-    /**
-     * Initializes clicked album and loads the AlbumDetailFragment.
-     */
-//    fun goToAlbumDetail(album: Album) {
-//        val frag = AlbumDetailFragment()
-//        frag.initiateAlbum(album)
-//        supportFragmentManager.beginTransaction().replace(com.hogent.dikkeploaten.R.id.fragment_container, frag)
-//            .addToBackStack(null).commit()
-//    }
-
-    /**
-     * Inflates the settings menu item into the actionbar.
-     */
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(com.hogent.dikkeploaten.R.menu.settings_menu, menu)
-        return true
-    }
-
-    /**
-     * Handles actions for settings menu.
-     */
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            com.hogent.dikkeploaten.R.id.action_settings -> {
-                val intent = Intent(this, SettingsActivity::class.java)
-                startActivity(intent)
-                return true
             }
         }
-
-        return super.onOptionsItemSelected(item)
     }
 
 }
