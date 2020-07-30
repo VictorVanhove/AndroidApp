5239ea85b61ba782ea66d31ccfbbe783ee83fb5b
Victor Vanhove <victorvanhove@hotmail.com>
2020-07-30 20:53:48 +0200

    Removed duplicate bindingAdapter imageUrl + updated Glide

--- app/src/main/java/com/hogent/dikkeploaten/BindingAdapters.kt
+++ app/src/main/java/com/hogent/dikkeploaten/BindingAdapters.kt
@@ -2,10 +2,12 @@ package com.hogent.dikkeploaten
 
 import android.view.View
 import android.widget.ImageView
+import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.RequestOptions
 import com.hogent.dikkeploaten.adapters.AlbumAdapter
 import com.hogent.dikkeploaten.database.DatabaseAlbum
 import com.hogent.dikkeploaten.viewmodels.ApiStatus
@@ -34,16 +36,15 @@ fun bindRecyclerView(recyclerView: RecyclerView, data: List<DatabaseAlbum>?) {
  */
 @BindingAdapter("imageUrl")
 fun bindImage(imgView: ImageView, imgUrl: String?) {
-    Glide.with(imgView.context).load(imgUrl).into(imgView)
-}
-
-@BindingAdapter("imageFromUrl")
-fun bindImageFromUrl(view: ImageView, imageUrl: String?) {
-    if (!imageUrl.isNullOrEmpty()) {
-        Glide.with(view.context)
-            .load(imageUrl)
-            .transition(DrawableTransitionOptions.withCrossFade())
-            .into(view)
+    imgUrl?.let {
+        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+        Glide.with(imgView.context)
+            .load(imgUri)
+            .apply(
+                RequestOptions()
+                .placeholder(R.drawable.loading_animation)
+                .error(R.drawable.ic_broken_image))
+            .into(imgView)
     }
 }
 
