95f0a7c4c30fcee28bf348e1f35718bfc09ad48a
Victor Vanhove <victorvanhove@hotmail.com>
2020-08-15 12:02:42 +0200

    Added broken image when image is not loading

--- app/src/main/java/com/hogent/dikkeploaten/BindingAdapters.kt
+++ app/src/main/java/com/hogent/dikkeploaten/BindingAdapters.kt
@@ -37,6 +37,7 @@ fun bindImage(imgView: ImageView, imgUrl: String?) {
             .apply(
                 RequestOptions()
                     .placeholder(R.drawable.loading_animation)
+                    .error(R.drawable.ic_broken_image)
             )
             .into(imgView)
     }
